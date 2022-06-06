0ec3362f70a926fb11f41ddae703b3b2489325cb
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 12:49:25 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return(0);
+}
