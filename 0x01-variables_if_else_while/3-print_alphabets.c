14d90342824790665f63248ec97f05872dd5907c
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 16:59:51 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'a', m <= 'z', m++);
+		putchar(m);
+	for (m = 'A', m <= 'Z', m++);
+		putchar(m);
+	putchar('\n');
+	return(0);
+}
