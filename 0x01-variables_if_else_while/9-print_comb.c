5231cb6c61a2a9df9b39d9a122ae252ebe6942cb
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 18:56:14 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of zero digit numbers
+ *
+ * Return: Always return 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = o; number <= 9, number++)
+	{
+	putchar(number + '0');
+	if (number < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
