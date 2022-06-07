c7db919be9426c2ffe0b132304456e52fd32ecd7
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 17:56:33 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry 
+ *
+ * return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	int num_base10;
+
+	for (num_base10 = 0, num_base10 < 10, num_base10++)
+		printf("%d\n", num_base10);
+	putchar('\n');
+	return (o);
+}
