a5bc4b3c98b2a47cb2e1a204a859cd785ee32775
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 17:03:05 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char m;
 
-	for (m = 'a', m <= 'z', m++);
+	for (m = 'a'; m <= 'z'; m++)
 		putchar(m);
-	for (m = 'A', m <= 'Z', m++);
+	for (m = 'A'; m <= 'Z'; m++)
 		putchar(m);
 	putchar('\n');
 	return(0);
