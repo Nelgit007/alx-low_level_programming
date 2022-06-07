e66d9cdb93f1fafcfa05f87ac19498eb8e646749
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 17:34:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for ( a = 'a'; a <= 'z'; a++)
+		
+	if (a != 'q' && a != 'e')
+		putchar(a);
+	putchar('\n')
+	return (0);
+}
