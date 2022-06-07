ae2ee1c90736ccb63ba92fdb5762878ce466eed6
Nelgit007 <osagienelson24@gmail.com>
2022-06-06 19:42:10 -0700

    commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible different combination of three digits
+ *
+ * Return: Always return 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+	for (n = 48; n < 58; n++)
+	{
+	if (n > i)
+	{
+	for (k = 48; k < 58; k++)
+	{
+	if (k > n)
+	{
+	putchar(i);
+	putchar(n);
+	putchar(k);
+	if (i == 55 && n == 56 && k == 57)
+	{
+	break;
+
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
