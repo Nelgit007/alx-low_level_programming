a643d60be8162383b0873e9e9e0a97b2ad4e6130
Nelgit007 <osagienelson24@gmail.com>
2022-06-12 05:01:10 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * main - Check Discription
  *
  * Description: This program prints the word _putchar followed by a new line.
- * Return 0 (success)
+ * Return: Always return 0 (success)
  */
 int main(void)
 {
