c3574c97cf8a96d91907d5082b3cb5ef796ac8df
Nelgit007 <osagienelson24@gmail.com>
2022-06-11 22:28:48 -0700

    Update main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabetx10(void);
+
+int _islower(int c);
+
+int _is alpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+#endif
+
+
