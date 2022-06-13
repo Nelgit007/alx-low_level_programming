3434e3ab5d68f0b711f21888fcebeeb0687a9d7d
Nelgit007 <osagienelson24@gmail.com>
2022-06-12 19:48:35 -0700

    first commit 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print a - z ten times
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(10 * c);
+		c++;
+	}
+	_putchar('\n');
+}
