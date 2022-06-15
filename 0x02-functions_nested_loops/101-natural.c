2743927b4d1ac4cc52b0adf98f076913ab5a2392
Nelgit007 <osagienelson24@gmail.com>
2022-06-15 02:04:14 -0700

    First commit 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Multiples - The sum of multiples
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+	if (c % 3 == 0 || c % 5 == 0)
+	{
+	sum += c;
+	}
+	c++;
+	}
+	printf("%i \n", sum);
+	return (0);
+}
