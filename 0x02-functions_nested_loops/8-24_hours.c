8e08f08706725e5bddebb4c09e0dc79a3eb86cad
Nelgit007 <osagienelson24@gmail.com>
2022-06-14 20:18:25 -0700

    first commit 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * lack_bauer - prints every minute of the day from 00:00 to 23:59
+ *
+ * Return: ALways return 0.
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	if (a = 0; a < 23; a++)
+	{
+	if (b = 0; b < 59; b++)
+	}
+	{
+	_putchar(a / 10 + '0');
+	_putchar(a % 10 + '0');
+	_putchar(':');
+	_putchar(b / 10 + '0');
+	_putchar(b % 10 + '0');
+	_putchar('\n');
+	}
+}
