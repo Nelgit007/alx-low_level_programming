b08b29f7deee612e1bda7b3bde5d9b627becd531
Nelgit007 <osagienelson24@gmail.com>
2022-06-14 21:35:22 -0700

    first commit 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * addition -adds two int and prints result
+ *
+ * @l: first int value
+ * @m: second int value
+ * Return: Always return 0.
+ */
+
+int add(int l, int m)
+{
+	return (l + m);
+}
