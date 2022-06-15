99438cc92e5b81b90de014b04330abfc6d1fef5b
Nelgit007 <osagienelson24@gmail.com>
2022-06-14 20:22:05 -0700

    Updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,16 +11,16 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	if (a = 0; a < 23; a++)
+	if (a = 0; a <= 23; a++)
 	{
-	if (b = 0; b < 59; b++)
+	if (b = 0; b <= 59; b++)
 	}
 	{
-	_putchar(a / 10 + '0');
-	_putchar(a % 10 + '0');
-	_putchar(':');
-	_putchar(b / 10 + '0');
-	_putchar(b % 10 + '0');
-	_putchar('\n');
+	_putchar (a / 10 + '0');
+	_putchar (a % 10 + '0');
+	_putchar (':');
+	_putchar (b / 10 + '0');
+	_putchar (b % 10 + '0');
+	_putchar ('\n');
 	}
 }
