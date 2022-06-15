89e806ab9773daf7434c744014b8efdf2f973cd4
Nelgit007 <osagienelson24@gmail.com>
2022-06-15 13:17:33 -0700

    Updated commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
- * main - causes and infinite loop
- * return: 0
+ * main - causes an infinite loop
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
+
+
 	printf("Infinite loop incoming :(\n");
 
+
+
 	i = 0;
+
+
 
 	/*while (i < 10)*/
 
@@ -21,7 +27,11 @@ int main(void)
 
 	/*}*/
 
+
+
 	printf("Infinite loop avoided! \\o/\n");
+
+
 
 	return (0);
 }
