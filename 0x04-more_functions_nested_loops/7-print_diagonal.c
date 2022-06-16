7c89295153a8fd7811fc953feed7875ec6247c89
Nelgit007 <osagienelson24@gmail.com>
2022-06-16 11:10:30 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints forward slash
+ * @n: parameter
+ */
+
+void print_diagonal(int n)
+{
+	int = 0;
+	int j;
+
+	if (n > 0)
+	{
+	for (; i < n; i++)
+	{
+	for (j = 0; j < 1; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
