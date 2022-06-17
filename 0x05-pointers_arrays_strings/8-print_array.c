b9f30f36a31db444b173137b58f344c02d2cc0ea
Nelgit007 <osagienelson24@gmail.com>
2022-06-16 21:28:52 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_arrays - prints array
+ * @a: param
+ * @n: param
+ */
+
+void print_array(int *a, int *n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+	printf("%d", *(a + i));
+	if (i != (n - 1))
+		printf(", ");
+	}
+	printf('\n');
+}
