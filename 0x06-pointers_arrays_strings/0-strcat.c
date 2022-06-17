eeaa7198ddaa88c13b9a972ac5211b53fd65edf0
Nelgit007 <osagienelson24@gmail.com>
2022-06-17 04:41:16 -0700

    first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,12 +3,12 @@
 /**
  * _strcat - concatenate two strings
  * @dest: A pointer to a character that will change
- * @src: A pointer to a character that will change
+ * @src: A pointer to a character that will also change
  * @n: value
  * return: dest
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
@@ -19,13 +19,13 @@ char *_strcat(char *dest, char *src, int n)
 	}
 
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (src[j] != '\0')
 	{
 	dest[i] = src[j];
 	j++;
 	i++;
 	}
-
+	
 	dest[i] = '\0';
 
 	return (dest);
