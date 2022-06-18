2bb0b6da45d27205aaa43395554ec7c88235ff2a
Nelgit007 <osagienelson24@gmail.com>
2022-06-17 21:24:45 -0700

    Updated commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -27,12 +27,14 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n / 2; i++)
 	{
 	
-
-		c = *end_c;
-		*end_c = *start_c;
-		*start_c = c;
-
-		start_c++;
-		end_c++;
+	
+	
+	c = *end_c;
+	*end_c = *start_c;
+	*start_c = c;
+	
+	
+	start_c++;
+	end_c++;
 	} 
 }
