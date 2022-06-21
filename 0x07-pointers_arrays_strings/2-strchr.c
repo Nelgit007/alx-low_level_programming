9c12f1be8c0044155aeb078643f1790d13818b89
Nelgit007 <osagienelson24@gmail.com>
2022-06-21 10:02:50 -0700

    Updared commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[index] >= '\0'; index++)
+	for (index = 0; s[index] != '\0'; index++)
 	{
 	if (s[index] == c)
 		return (s + index);
 	}
+	if (s[index] == c)
+		return (s + index);
 
 	return ('\0');
 }
