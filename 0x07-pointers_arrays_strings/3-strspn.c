753f9e9aa7376b1055e1044b6b22e71b42985500
Nelgit007 <osagienelson24@gmail.com>
2022-06-21 20:13:17 -0700

    first commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
+
 /**
- * _strspn - Gets the lenght of a prifix substring.
+ * _strspn - Gets the length of a prefix substring.
  * @s: The string to be searched.
- * @accept: The prifix to be measured.
+ * @accept: The prefix to be measured.
  *
- * Return: The number of bytes in s which 
- * consist only of bytes from accept
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
  */
 
-unsigned int _strspn(char *s, char *accept)
 
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
 	int index;
-	
+
+
 	while (*s)
 	{
 		for (index = 0; accept[index]; index++)
@@ -24,13 +26,16 @@ unsigned int _strspn(char *s, char *accept)
 				bytes++;
 				break;
 			}
-	
+
+
 			else if (accept[index + 1] == '\0')
 				return (bytes);
 		}
-	
+
+
 		s++;
 	}
-	
+
+
 	return (bytes);
 }
